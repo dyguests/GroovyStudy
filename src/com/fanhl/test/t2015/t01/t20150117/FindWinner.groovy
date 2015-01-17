8ff25bdb134372e03761d45832634bbd4d793b78
fanhl <Dyguest0315>
2015-01-18 00:45:19 +0800

    一道练习题

---
+++ src/com/fanhl/test/t2015/t01/t20150117/FindWinner.groovy
@@ -0,0 +1,6 @@
+def lst = new File("winner.txt").text.split("\n").collect { it.split(",") }
+def findCountry = { l, y -> println "年份:$y,国家:${l.grep { it[0] == "$y" }.collect { it[2] }}" }.curry lst
+def findYear = { l, c -> println "国家:$c,年份:${l.grep { it[2] == c }.collect { it[0] }}" }.curry lst
+
+findCountry 1930
+findYear "巴西"
