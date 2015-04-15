3fd8cc8d3d8e0ab8afa24dcc79d02635c788ead3
fanhl <Dyguest0315>
2015-04-15 13:12:28 +0800

    程序员编程艺术：第二章、字符串是否包含问题

--- src/com/fanhl/test/t2015/t03/t20150415/StringContainChecker.groovy
+++ src/com/fanhl/test/t2015/t03/t20150415/StringContainChecker.groovy
@@ -2,8 +2,8 @@ package com.fanhl.test.t2015.t03.t20150415
 //时间为O(m+n),空间上只用了一个int变量(若要分大小写就用long变量就行)
 def contain(String father, String son) {
     int checker = 0
-    for (char c : son) checker |= 1 << (c - 97)//根据son对checker对应位1
-    for (char c : father) checker &= ~(1 << (c - 97))//根据father对checker对应位置0
+    for (char c : son) checker |= 1 << (c - 97)//根据son对checker对应位 置1
+    for (char c : father) checker &= ~(1 << (c - 97))//根据father对checker对应位 置0
     !checker//checker为0时表示完全包含
 }
 
